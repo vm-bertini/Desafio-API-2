27e08656ad868257d39e2ae365c767da174deaef
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-06-10 00:36:27 -0300

    Criando as primeiras funções Get para os produtos

--- src/controllers/produtos_controller.js
+++ src/controllers/produtos_controller.js
@@ -1,9 +1,32 @@
-class produtos_controller {
-    async hello (req,res) {
-        res.status(200).json(req)
-    }
-}
+const connection = require('../../Database/database_connection');
 
-//olá tudo bem
+const resposta_400 = require('../Respostas/400')
+
+class produtos_controller {
+    async read (req,res) {
+        //Condicional para quanto se quer apenas um produto baseado em seu id
+        if (req.params.id != undefined) 
+            {connection.query('SELECT * FROM teste.produtos Where id =' + req.params.id, function (err, rows, fields) {
+
+                if (err || rows.length == 0) 
+                {return res.status(404).json(resposta_400.Not_Found)}
+                
+                return res.status(200).json(rows)
+                })
+            }
+        //Condicional para quanto se quer todos os produtos
+        else{
+            {connection.query('SELECT * FROM teste.produtos', function (err, rows, fields) {
+
+                if (err || rows.length == 0) 
+                {return res.status(404).json(resposta_400.Not_Found)}
+
+                return res.status(200).json(rows)
+
+                })
+            }   
+        }
+    };
+}
 
 module.exports = new produtos_controller()
