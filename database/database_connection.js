976931f3a4179dc80edcfb645a4634b0500b1598
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-06-11 06:55:39 -0300

    Generalizando arquivo

--- database/database_connection.js
+++ database/database_connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'vm_bertini@2002',
+    host: '',
+    user: '',
+    password: '',
     database: 'Banco_Desafio_API',
     multipleStatements: true
   })
